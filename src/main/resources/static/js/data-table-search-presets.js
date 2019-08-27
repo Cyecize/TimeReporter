aaf84aa0f7b33fc84e1160f5b9eb658dba1c2849
cyecize <ceci2205@abv.bg>
2019-08-27 18:02:06 +0300

    Moved search button configs to a single file.

---
+++ src/main/resources/static/js/data-table-search-presets.js
@@ -0,0 +1,17 @@
+const SearchPresets = (() => {
+
+    return {
+        project(table, container) {
+            TableUtils.createSearchBarForTable(table, container, {
+                showLabel: true,
+                hideOptions: false,
+                options: [
+                    'Id',
+                    'Project Name',
+                    'Owner'
+                ],
+                searchFieldId: 'searchProjectsField',
+            });
+        },
+    };
+})();
