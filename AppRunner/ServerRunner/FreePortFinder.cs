b5e713c498634d24306d13307ef1fea4406b20ff
cyecize <ceci2205@abv.bg>
2019-08-30 18:27:42 +0300

    Added C# app to help with running the app in production.

---
+++ AppRunner/ServerRunner/FreePortFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerRunner
+{
+    static class FreePortFinder
+    {
+        public static int FindFreePort(int startingPort)
+        {
+            for (int port = startingPort; port < startingPort + 100; port++)
+            {
+                if (IsPortFree(port))
+                {
+                    return port;
+                }
+            }
+
+            throw new Exception($"Port {startingPort} is taken and none of the next following 100 are free!");
+        }
+
+        private static bool IsPortFree(int port)
+        {
+            IPAddress ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            try
+            {
+                TcpListener tcpListener = new TcpListener(ipAddress, port);
+                tcpListener.Start();
+                tcpListener.Stop();
+                return true;
+            }
+            catch (SocketException ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AppRunner/ServerRunner/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Timers;
+
+namespace ServerRunner
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            const int initialPort = 8000;
+            const int initialAppLoadedListenerPort = 4000;
+
+            Console.WriteLine("\n\nStarting Time Reporter\n\n");
+            Process process = new Process();
+
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+
+            process.Start();
+
+            int appPort = FreePortFinder.FindFreePort(initialPort);
+            int appLoadedListenerPort = FreePortFinder.FindFreePort(initialAppLoadedListenerPort);
+
+            process.StandardInput.WriteLine(@"jre\bin\java.exe -cp "".;./bin/javache-api-1.2.6.jar;"" com.cyecize.StartUp " + appPort + " " + appLoadedListenerPort);
+
+            process.StandardInput.Flush();
+            process.StandardInput.Close();
+
+            process.OutputDataReceived += (sender, eventArgs) => { Console.WriteLine(eventArgs.Data); };
+            process.BeginOutputReadLine();
+
+            InitAppTimeout();
+            InitAppLoadedListener(new string[] { $"http://localhost:{appLoadedListenerPort}/" });
+        }
+
+        private static void InitAppTimeout()
+        {
+            Timer timer = new Timer();
+            timer.Interval = 10000;
+            timer.Elapsed += (sender, eventArgs) =>
+            {
+                Console.WriteLine("App did not start properly!");
+                Environment.Exit(1);
+            };
+
+            timer.Start();
+        }
+
+        private static void InitAppLoadedListener(string[] prefixes)
+        {
+            HttpListener listener = new HttpListener();
+
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+
+            listener.Start();
+
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+
+            HttpListenerResponse response = context.Response;
+            response.Redirect("https://www.google.com");
+
+            System.IO.Stream output = response.OutputStream;
+            output.WriteByte(0);
+
+            output.Close();
+            listener.Stop();
+            Environment.Exit(0);
+        }
+    }
+}
