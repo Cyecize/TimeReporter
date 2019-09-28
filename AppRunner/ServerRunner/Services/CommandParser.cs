bcc2a71ce3c28ff5567a69da1ecc9df359fac4d2
Tsvetelin Yakimov <ceci2205@abv.bg>
2019-09-29 01:08:17 +0300

    Added more command formatting features to C# AppRunner.

--- AppRunner/ServerRunner/Services/CommandParser.cs
+++ AppRunner/ServerRunner/Services/CommandParser.cs
@@ -6,10 +6,18 @@ namespace ServerRunner.Services
 {
     public class CommandParser : ICommandParser
     {
+
+        private ILocalConfigManager _configManager;
+
+        public CommandParser(ILocalConfigManager localConfig)
+        {
+            this._configManager = localConfig;
+        }
+
         public string ParseCommand(string command)
         {
             this.HandlePortFormatting(ref command);
-            //TODO: you can add more handlers.
+            this.HandleConfigFormatting(ref command);
 
             return command;
         }
@@ -25,6 +33,22 @@ namespace ServerRunner.Services
             {
                 int port = int.Parse(match.Groups["port"].Value);
                 command = regex.Replace(command, FreePortFinder.FindFreePort(port).ToString(), 1);
+
+                match = regex.Match(command);
+            }
+        }
+
+        private void HandleConfigFormatting(ref string command)
+        {
+            Regex regex = new Regex(@"{config:(?<keyName>\w{1,})}");
+            if (!regex.IsMatch(command)) return;
+
+            Match match = regex.Match(command);
+
+            while (match.Success)
+            {
+                string configVal = match.Groups["keyName"].Value;
+                command = regex.Replace(command, this._configManager.GetConfig(configVal), 1);
 
                 match = regex.Match(command);
             }
